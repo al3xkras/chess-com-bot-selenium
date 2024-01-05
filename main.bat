008c9ce8f2d45edd5ca346a6635e28c0c99e2324
al3xkras < >
2024-01-05 17:02:50 +0100

    [PR #1 https://github.com/Astr0Dev/chess-com-bot-selenium]
    Fix negative timer -> negative move delay issue:
    ```
    timer_ = max(0, timer_ - (wt + stockfish_t) * 1000)
    ```
    
    edit exception clause at <if not os.path.exists(stockfish_path)>
    
    edit exception clause at <async def main0 -> def loop>
    
    main.bat: set default move to e2e4

--- main.bat
+++ main.bat
@@ -1,1 +1,1 @@
-"./venv/Scripts/python" main.py --elo-rating -1 --game-timer-ms 60000 --first-move-w g1f3 --enable-move-delay
+"./venv/Scripts/python" main.py --elo-rating -1 --game-timer-ms 600 --first-move-w e2e4 --enable-move-delay
